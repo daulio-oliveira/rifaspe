bf6540c14cd47d588d7f3030704c9ede30599d4a
Daulio Oliveira <daulio.junior@gmail.com>
2020-06-22 22:01:40 -0300

    liberação de cors

--- src/main/java/com/cs/rifas/rifasaju/controller/PremioController.java
+++ src/main/java/com/cs/rifas/rifasaju/controller/PremioController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,6 +23,7 @@ import com.cs.rifas.rifasaju.service.PremioService;
 
 @RestController()
 @RequestMapping("/premio")
+@CrossOrigin
 public class PremioController {
 
 	@Autowired private PremioService service;
