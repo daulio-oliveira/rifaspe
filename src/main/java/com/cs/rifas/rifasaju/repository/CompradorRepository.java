b44a37accbebc62ba5c69a6141f13f1a0439e0d3
Daulio Oliveira <daulio.junior@gmail.com>
2020-06-24 16:22:33 -0300

    refactor

--- src/main/java/com/cs/rifas/rifasaju/repository/CompradorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cs.rifas.rifasaju.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cs.rifas.rifasaju.model.Comprador;
-
-public interface CompradorRepository extends JpaRepository<Comprador, String> {
-
-}
